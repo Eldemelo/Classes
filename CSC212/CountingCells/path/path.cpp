d0b5ba7bfce58e3192b5f7e52a7776eec705b343
Ethan DeMelo <ethan.demelo2000@gmail.com>
2024-04-03 23:30:01 -0400

    Matched expected output for path

--- CSC212/CountingCells/path/path.cpp
+++ CSC212/CountingCells/path/path.cpp
@@ -78,26 +78,107 @@ grid::grid(string fName, int rows, int cols){
         }
     }
     // Determine which conn type to use
-    // cout << blobCount(this->origin) << endl;
+    tuple<int, int, int> lowest = findLowest(this->origin);
+    cout << get<0>(lowest) << " " << get<1>(lowest) << " ";
+    tuple<int, int, int> highest = findHighest(this->origin);
+    cout << get<0>(highest) << " " << get<1>(highest) << " ";
+
+    // Move a pointer to the starting lowest node's coordinates
+    tempCell = this->origin;
+    for(int row = 1; row < get<0>(lowest); row++){
+        tempCell = tempCell->s;
+    }
+    for(int col = 1; col < get<1>(lowest); col++){
+        tempCell = tempCell->e;
+    }
+
+    // Find if the grid has a path and print yes or no accordingly
+    hasPath(tempCell, get<2>(lowest), get<2>(highest)) ? cout << "yes" : cout << "no";
 }
 
-pair<int, int> grid::findLowest(cells* cell){
+// Recursive method to determine if the values have a path from lowest to highest
+bool grid::hasPath(cells* cell, int lowest, int highest){
+    // Base case
+    if(!cell){
+        return false;
+    }
+    if(cell->value == highest){
+        return true;
+    }
+    cell->visited = true;
+    bool pathFound = false;
+    // Navigate each direction if possible
+    if(cell->n && cell->n->value >= cell->value && !cell->n->visited){
+        pathFound = hasPath(cell->n, cell->value, highest);
+    }
+    if(cell->e && cell->e->value >= cell->value && !cell->e->visited){
+        pathFound = hasPath(cell->e, cell->value, highest);
+    }
+    if(cell->s && cell->s->value >= cell->value && !cell->s->visited){
+        pathFound = hasPath(cell->s, cell->value, highest);
+    }
+    if(cell->w && cell->w->value >= cell->value && !cell->w->visited){
+        pathFound = hasPath(cell->w, cell->value, highest);
+    }
+    return pathFound;
+}
+
+
+// Method to find the coordinates of the lowest value
+tuple<int, int, int> grid::findLowest(cells* cell){
     int lowestValue = cell->value;
     int lowestRow = 1;
     int lowestCol = 1;
-    for(int i = 1; i <= this->rows; i++){
-        for(int j = 1; j <= this->cols; j++){
+    for(int row = 1; row <= this->rows; row++){
+        for(int col = 1; col <= this->cols; col++){
             if(cell->value < lowestValue){
                 lowestValue = cell->value;
-                lowestRow = i;
-                lowestCol = j;
+                lowestRow = row;
+                lowestCol = col;
             }
-            cell = cell->e;
+            // Don't move east on the last column
+            if(col != this->cols) {  
+                cell = cell->e;
+            }
         }
-        // Navigate back to the left
-        while(cell->w){
+        // Only move west cols - 1 times
+        for(int col = 1; col < this->cols; col++){
             cell = cell->w;
         }
-        cell = cell->s;
+        // Don't move south on the last row
+        if(row != this->rows) {
+            cell = cell->s;
+        }
     }
+    return make_tuple(lowestRow, lowestCol, lowestValue);
 }
+
+// Method to find the coordinates of the highest value
+tuple<int, int, int> grid::findHighest(cells* cell){
+    int highestValue = cell->value;
+    int highestRow = 1;
+    int highestCol = 1;
+    for(int row = 1; row <= this->rows; row++){
+        for(int col = 1; col <= this->cols; col++){
+            if(cell->value > highestValue){
+                highestValue = cell->value;
+                highestRow = row;
+                highestCol = col;
+            }
+            // Don't move east on the last column
+            if(col != this->cols) {  
+                cell = cell->e;
+            }
+        }
+        // Only move west cols - 1 times
+        for(int col = 1; col < this->cols; col++){
+            cell = cell->w;
+        }
+        // Don't move south on the last row
+        if(row != this->rows) {
+            cell = cell->s;
+        }
+    }
+    return make_tuple(highestRow, highestCol, highestValue);
+}
+
--- CSC212/CountingCells/path/path.h
+++ CSC212/CountingCells/path/path.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <string>
 #include <fstream>
 
@@ -25,6 +26,7 @@ class grid{
     public:
     grid();
     grid(string fName, int rows, int cols);
-    pair<int, int> findLowest(cells* cell);
-    pair<int, int> findHighest(cells* cell);
+    tuple<int, int, int> findLowest(cells* cell);
+    tuple<int, int, int> findHighest(cells* cell);
+    bool hasPath(cells* cell, int lowest, int highest);
 };
