67630c11b731eb9daa018b2e4603b565ba9cc8c7
Ethan DeMelo <ethan.demelo2000@gmail.com>
2024-04-10 22:15:18 -0400

    Pass 1d map

--- CSC212/SetsAndMaps/set_map/set_map.cpp
+++ CSC212/SetsAndMaps/set_map/set_map.cpp
@@ -19,7 +19,7 @@ int main(int argc, char* argv[]){
     // Create set to hold countries
     set<string> countrySet;
     // map<dataTypeForKey, dataTypeForValue> name;
-    map<string, string> countryMap;
+    map<string, int> countryMap;
     map<string, map<string, string>> nestedMap;
 
     // Read from a file
@@ -51,6 +51,44 @@ int main(int argc, char* argv[]){
     // Close the dataset reading
     readData.close();
 
+    // Re-open file for maps
+    ifstream readMapData(dataSet);
+    // Skip over the first line
+    getline(readMapData, currLine);
+    while(getline(readMapData, currLine)){
+        // cout << "Next line" << endl;
+        istringstream ss(currLine);
+        int col = 0;
+        string country;
+        string saveCountry;
+        while(getline(ss, sLine, ',')){
+            col++;
+            if(ss.peek() == '"'){
+                ss.get();
+                country = getCurrCol(&ss, sLine);
+                if(col == 3){
+                    // Save the country for future parsing
+                    saveCountry = country;
+                }
+            }
+            else if(col == 4){
+                saveCountry = sLine;
+            }
+            else if(col == 8){
+                // If the country already exists
+                if(countryMap.find(saveCountry) != countryMap.end()){
+                    // cout << "hello" << endl;
+                    countryMap[saveCountry] += stoi(sLine);
+                }
+                else{
+                    // cout << "Does not exist: " << saveCountry << " " << sLine << endl;
+                    countryMap.insert({saveCountry, stoi(sLine)});
+                }
+            }
+        }
+    }
+    readMapData.close();
+
     // Open the commands file
     ifstream commands(commandList);
     while(getline(commands, sLine)){
@@ -62,15 +100,22 @@ int main(int argc, char* argv[]){
             countrySet.find(sLine) != countrySet.end() ? cout << 1 << endl : cout << 0 << endl;
         }
         else if(sLine == "map"){
-            getline(ssLine, sLine, ' ');
-            cout << 0 << endl;
+            // Determine if the map contains a semi-colon
+            getline(ssLine, sLine, '\n');
+            cout << countryMap[sLine] << endl;
         } 
     }
 
 
-    cout << endl << "Set" << endl;
-    // Print every value in the set
-    for(set<string>::iterator mySet = countrySet.begin(); mySet != countrySet.end(); mySet++){
-        cout << *mySet << endl;
-    }
+    // cout << endl << "Set" << endl;
+    // // Print every value in the set
+    // for(set<string>::iterator mySet = countrySet.begin(); mySet != countrySet.end(); mySet++){
+    //     cout << *mySet << endl;
+    // }
+
+    // cout << endl << "Map" << endl;
+    // // Print every value in the set
+    // for(set<string>::iterator mySet = countrySet.begin(); mySet != countrySet.end(); mySet++){
+    //     cout << countryMap[*mySet] << endl;
+    // }
 }
